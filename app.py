22aac4766e410814e2a95408316385e5e8d37ebe
Roman <roman985475612@gmail.com>
2020-09-10 12:20:53 +0300

    Chapter 3 complated

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from functools import wraps
+
 from flask import Flask
 from flask import flash
 from flask import redirect
 from flask import request
 from flask import url_for
-from flask import sessions
+from flask import session
 from flask import logging
 from flask import render_template
 from flask_mysqldb import MySQL
@@ -29,6 +31,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -75,11 +78,80 @@ def register():
         mysql.connection.autocommit(on=True)
         cur.close()
 
-        flash('Вы успешно зарегистрированы!', 'success')
+        session['logged_in'] = True
+        session['username'] = form.username.data
+        session['name'] = form.name.data
 
+        flash('Вы успешно зарегистрированы!', 'success')
         return redirect(url_for('index'))
 
     return render_template('register.html', form=form)
+
+
+class LoginForm(Form):
+    username = StringField('Имя пользователя', [Length(min=4, max=25)])
+    password = PasswordField('Пароль', [DataRequired()])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password_candidate = form.password.data
+
+        cur = mysql.connection.cursor()
+        query = "SELECT name, password FROM users WHERE username = %s"
+        result = cur.execute(query, [username])
+
+        if result > 0:
+            data = cur.fetchone()
+            name = data['name']
+            password = data['password']
+
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                session['name'] = name
+
+                flash('Здравствуйте, {}!'.format(name), 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                app.logger.info('PASSWORD NOT MATCHED')
+                flash('Неправельный логин или пароль!', 'danger')
+
+            cur.close()
+        else:
+            app.logger.info('NO USER')
+            flash('Неправельный логин или пароль!', 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    name = session['name']
+    session.clear()
+    flash('До свидания, {}'.format(name), 'success')
+    return redirect(url_for('index'))
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Вам необходима войти!', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
