169cffb1ec9d0bf612c505eb8a1c6af650270398
Roman <roman985475612@gmail.com>
2020-09-11 09:56:46 +0300

    Adding a dynamic title

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ from flask import redirect
 from flask import request
 from flask import url_for
 from flask import session
-# from flask import logging
 from flask import render_template
 from flask_mysqldb import MySQL
-
-# import email_validator
 
 from passlib.hash import sha256_crypt
 
